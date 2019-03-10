6d1a58e5319c0119dc884710009c7ed76fb8052e
João Rocha <joaovrocha16@gmail.com>
2019-03-10 16:03:25 -0300

    versão 0.1.3 adição do FrmFrequentadorPesquisa

--- src/Estatistica/controle/ControleFrequentador.java
+++ src/Estatistica/controle/ControleFrequentador.java
@@ -5,6 +5,11 @@
  */
 package Estatistica.controle;
 
+import Estatistica.modelo.InfosTabela;
+import java.util.List;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author User
@@ -12,5 +17,26 @@ package Estatistica.controle;
 public class ControleFrequentador {
     
     
+
+    public DefaultTableModel preencherJTableIntervalo(List<InfosTabela> info, JTable tabela) {
+        //Aqui carrego minha lista
+        DefaultTableModel modeloTable = (DefaultTableModel) tabela.getModel();
+
+        //Aqui verifico se a jTable tem algum registo se tiver eu deleto
+        while (modeloTable.getRowCount() > 0) {
+            modeloTable.removeRow(0);
+        }
+
+        //Aqui eu adiciono cada linha da lista na jTable
+        for (InfosTabela inf : info) {
+            modeloTable.addRow(new Object[]{inf.getIntervalo(),
+                inf.getKi(), inf.getFa(),
+                inf.getFr(), inf.getFrPCento(),
+                inf.getFac(), inf.getFrPCento()});
+        }
+        return modeloTable;
+    }
     
+    
+
 }
